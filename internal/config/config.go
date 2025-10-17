1531ecc12c4207f49ec17b045d9b40d14c5c73cf
jaam8 <jaam8@yandex.ru>
2025-10-17 12:47:50 +0700

    update config

--- internal/config/config.go
+++ internal/config/config.go
@@ -10,28 +10,29 @@ import (
 )
 
 type Config struct {
-	Kafka    kafka.Config    `yaml:"kafka" env-prefix:"KAFKA_"`
-	Redis    redis.Config    `yaml:"redis" env-prefix:"REDIS_"`
-	Postgres postgres.Config `yaml:"postgres" env-prefix:"POSTGRES_"`
-	Service  ServiceConfig   `yaml:"service" env-prefix:"SERVICE_"`
+	Kafka          kafka.Config    `yaml:"kafka" env-prefix:"KAFKA_"`
+	Redis          redis.Config    `yaml:"redis" env-prefix:"REDIS_"`
+	Postgres       postgres.Config `yaml:"postgres" env-prefix:"POSTGRES_"`
+	Service        AppConfig       `yaml:"service" env-prefix:"APP_"`
+	LogLevel       string          `yaml:"log_level" env:"LOG_LEVEL" env-default:"info"`
+	MigrationsPath string          `yaml:"migrations_path" env:"MIGRATIONS_PATH" env-default:"./migrations"`
 }
 
-type ServiceConfig struct {
-	Port           uint16 `yaml:"port" env-prefix:"PORT"`
-	KafkaTopic     string `yaml:"kafka_topic" env-prefix:"KAFKA_TOPIC"`
-	KafkaGroupID   string `yaml:"kafka_group_id" env-prefix:"KAFKA_GROUP"`
+type AppConfig struct {
+	Port           uint16 `yaml:"port" env:"PORT" env-default:"8080"`
+	KafkaTopic     string `yaml:"kafka_topic" env:"KAFKA_TOPIC"`
+	KafkaGroupID   string `yaml:"kafka_group_id" env:"KAFKA_GROUP_ID"`
 	RedisDB        int    `yaml:"redis_db" env:"REDIS_DB" env-default:"0"`
-	BatchSize      int    `yaml:"batch_size" env:"BATCH_SIZE"`
-	FlushTimeout   int    `yaml:"flush_timeout" env:"FLUSH_TIMEOUT"`
+	BatchSize      int    `yaml:"batch_size" env:"BATCH_SIZE" env-default:"1"`
+	FlushTimeout   int    `yaml:"flush_timeout" env:"FLUSH_TIMEOUT" env-default:"1"`
 	MaxRetries     uint   `yaml:"max_retries" env:"MAX_RETRIES"`
 	BaseRetryDelay int    `yaml:"base_retry_delay" env:"BASE_RETRY_DELAY"`
-	MigrationsPath string `yaml:"migrations_path" env:"MIGRATIONS_PATH"`
 }
 
 func New() (Config, error) {
 	var cfg Config
 	// docker workdir - app/
-	if err := cleanenv.ReadConfig("../configs/config.yaml", &cfg); err != nil {
+	if err := cleanenv.ReadConfig(".env", &cfg); err != nil {
 		fmt.Println(err.Error())
 		if err := cleanenv.ReadEnv(&cfg); err != nil {
 			return Config{}, fmt.Errorf("failed to read env vars: %v", err)
